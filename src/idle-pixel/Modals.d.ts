27a4a68ab15acc159b4eeb573d3afa2b5dd13ab1
Andy <sxxe@gmx.de>
2023-03-13 16:49:14 +0100

    make logs in woodcutting overview tile clickable, to add heat to the oven

--- src/idle-pixel/Modals.d.ts
+++ src/idle-pixel/Modals.d.ts
@@ -1,6 +1,7 @@
 declare global {
     class Modals {
         static clicks_rocket: () => void
+        static clicks_oven_log: (log: string) => void
         static open_input_dialogue_with_value: (type: string, action1: string, text: string, value: number, action2: string) => void
     }
 }
--- src/overview/woodcutting/LogDisplay.tsx
+++ src/overview/woodcutting/LogDisplay.tsx
@@ -1,5 +1,6 @@
 import IPimg from "../../util/IPimg";
 import { useNumberItemObserver } from "../setItems/useSetItemsObserver";
+import { MouseEvent } from "react";
 
 interface Props {
   log: string;
@@ -9,6 +10,9 @@ const id = "LogDisplay";
 const LogDisplay = ({ log }: Props) => {
   const [amount] = useNumberItemObserver(log, id);
 
+  const onLogClick = (event: MouseEvent) => {
+    Modals.open_input_dialogue_with_value(log, "Add Heat", "<span class='font-large'>Add heat to your oven.</span><br /><br /><span class='color-grey'>Gain <img src='https://d1xsc8x7nc5q8t.cloudfront.net/images/heat.png' /> " + Cooking.getHeatPerLog(log) +" heat per log.</span><br /><br />", amount, "ADD_HEAT");
+  };
 
   return amount > 0 ? (
     <div
@@ -23,7 +27,9 @@ const LogDisplay = ({ log }: Props) => {
       <IPimg
         name={log}
         size={30}
+        onClick={onLogClick}
         title={Items.get_pretty_item_name(log)}
+        role={"button"}
       />
       <span>{amount}</span>
     </div>
